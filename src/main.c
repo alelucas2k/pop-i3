e8baaa2eb199dab15064519d93c62828e281294d
Alessandro Lucas <92497473+alelucas2k@users.noreply.github.com>
2023-03-01 17:45:12 -0300

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,52 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f4xx.h"
+#include "Utility.h"
+
+
+int main(void)
+{
+
+	Utility_Init(); //habilita as funções especiais
+
+	RCC -> AHB1ENR |= 0b10001;
+
+	GPIOA -> MODER |= 0b01<<12;
+	GPIOA -> MODER |= 0b01<<14;
+	GPIOE -> MODER |= 0B01;
+
+	while(1)
+	{
+		GPIOA -> ODR &= ~(1<<6);
+		GPIOA -> ODR |= 1 << 7;
+		GPIOE -> ODR &= ~(1);
+		Delay_ms(1000);
+		GPIOA -> ODR |= 1 << 6;
+		GPIOA -> ODR &= ~(1<<7);
+		GPIOE -> ODR &= ~(1);
+		Delay_ms(1000);
+		GPIOA -> ODR |= 1<<6;
+		GPIOA -> ODR |= 1 << 7;
+		GPIOE -> ODR &= ~(1);
+		Delay_ms(50);
+		GPIOE -> ODR |= 1;
+		Delay_ms(50);
+		GPIOE -> ODR &= ~(1);
+		Delay_ms(50);
+		GPIOE -> ODR |= 1;
+		Delay_ms(50);
+		//Delay_ms(1000);
+
+	}
+
+
+}
